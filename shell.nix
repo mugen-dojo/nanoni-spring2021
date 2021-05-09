21efc17e378c2c567fd95fcd94867ab4992602d2
Nano <nanoni@riseup.net>
2021-05-09 12:09:52 +0200

    Nix n’aime pas les espaces

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/00f9323c2717e4b2ed0b3c38c107a75de4198562.tar.gz") {} }:
 
 pkgs.mkShell {
-    name = "Les Nanoni Bots";
+    name = "LesNanoniBots";
     buildInputs = with pkgs; [
         git
         boot
