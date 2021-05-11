7f8c7569962e02a809966d3a9996c675c60b4f10
Nano <nanoni@riseup.net>
2021-05-11 18:17:50 +0200

    WIP: extraction des expressions lié au parsing

--- src/bots/naive.clj
+++ src/bots/naive.clj
@@ -3,51 +3,58 @@
 
 (defn output [msg] (println msg) (flush))
 (defn debug [msg] (binding [*out* *err*] (println msg) (flush)))
+
+(defn parse-stdin!
+  "Helper function to reduce the redundant mess that is parsing Codingame’s data"
+  [val-name]
+  ; There are only two kinds of inputs:
+  ;   a single integer or a list of integers
+  ; as such, we just have to test if there is spaces in the provided input
+  (let [input (read-line)]
+    (debug (str/join " | " ["parse-stdin! " val-name input]))
+    (cond
+      (nil? (re-find #" " input)) (Integer/parseInt input)
+      :else (map #(Integer/parseInt %) (str/split input #" ")))))
+
+
 (defn initialize-state! []
-  (let [numberOfCells (Integer/parseInt (read-line))]
-    ; index: 0 is the center cell, the next cells spiral outwards
-    ; richness: 0 if the cell is unusable, 1-3 for usable cells
-    ; neigh0: the index of the neighbouring cell for each direction
+  (let [numberOfCells (parse-stdin! "numberOfCells")]
     (dotimes [i numberOfCells]
-      ; ignoring the neigh values for now
-      (let [
-            [index richness] (map #(Integer/parseInt %) (str/split (read-line) #" "))])))
-  (defn run [& args]
-    (initialize-state!)
-    (while true
-      (let [; the game lasts 24 days: 0-23
-            day (Integer/parseInt (read-line))
+      (parse-stdin! "tree"))))
+
+(defn run [& args]
+  (initialize-state!)
+  (while true
+    (let [; the game lasts 24 days: 0-23
+          day (parse-stdin! "day")
             ; the base score you gain from the next COMPLETE action
-            nutrients (Integer/parseInt (read-line))
+          nutrients (parse-stdin! "nutrients")
             ; sun: your sun points
             ; score: your current score
-            [sun score] (map #(Integer/parseInt %) (str/split (read-line) #" "))
+          [sun score] (parse-stdin! "sun score")
             ; oppSun: opponent's sun points
             ; oppScore: opponent's score
             ; oppIsWaiting: whether your opponent is asleep until the next day
-            [oppSun oppScore oppIsWaiting] (str/split (read-line) #" ")
-            oppSun (Integer/parseInt oppSun)
-            oppScore (Integer/parseInt oppScore)
-            oppIsWaiting (not= "0" oppIsWaiting)
+          [oppSun oppScore oppIsWaiting] (str/split (read-line) #" ")
+          oppSun (Integer/parseInt oppSun)
+          oppScore (Integer/parseInt oppScore)
+          oppIsWaiting (not= "0" oppIsWaiting)
             ; the current amount of trees
-            numberOfTrees (Integer/parseInt (read-line))]
-        (dotimes [i numberOfTrees]
-          (let [; cellIndex: location of this tree
+          numberOfTrees (parse-stdin! "numberOfTrees")]
+      (dotimes [i numberOfTrees]
+        (let [; cellIndex: location of this tree
                 ; size: size of this tree: 0-3
                 ; isMine: 1 if this is your tree
                 ; isDormant: 1 if this tree is dormant
-                [cellIndex size isMine isDormant] (str/split (read-line) #" ")
-                cellIndex (Integer/parseInt cellIndex)
-                size (Integer/parseInt size)
-                isMine (not= "0" isMine)
-                isDormant (not= "0" isDormant)]))
-        (let [; all legal actions
-              numberOfPossibleActions (Integer/parseInt (read-line))]
-          (dotimes [i numberOfPossibleActions]
-            (let [; try printing something from here to start with
-                  possibleAction (read-line)]))
-
-          ; (debug "Debug messages...")
-
+              [cellIndex size isMine isDormant] (str/split (read-line) #" ")
+              cellIndex (Integer/parseInt cellIndex)
+              size (Integer/parseInt size)
+              isMine (not= "0" isMine)
+              isDormant (not= "0" isDormant)]))
+      (let [; all legal actions
+            numberOfPossibleActions (parse-stdin! "numberOfPossibleActions")]
+        (dotimes [i numberOfPossibleActions]
+          (let [; try printing something from here to start with
+                possibleAction (read-line)]))
           ; GROW cellIdx | SEED sourceIdx targetIdx | COMPLETE cellIdx | WAIT <message>
-          (output "WAIT"))))))
+        (output "WAIT")))))
