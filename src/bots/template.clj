b60a4cf3d103699e32297537a977bf92016456eb
Nano <nanoni@riseup.net>
2021-05-11 09:58:29 +0200

    Ajout du template fourni par Codingame (berk)

---
+++ src/bots/template.clj
@@ -0,0 +1,51 @@
+(ns bots.template
+  (:require [clojure.string :as str]))
+
+(defn output [msg] (println msg) (flush))
+(defn debug [msg] (binding [*out* *err*] (println msg) (flush)))
+
+(defn run [& args]
+  (let [; 37
+        numberOfCells (Integer/parseInt (read-line))]
+    (dotimes [i numberOfCells]
+      (let [; index: 0 is the center cell, the next cells spiral outwards
+            ; richness: 0 if the cell is unusable, 1-3 for usable cells
+            ; neigh0: the index of the neighbouring cell for each direction
+            [index richness neigh0 neigh1 neigh2 neigh3 neigh4 neigh5] (map #(Integer/parseInt %) (str/split (read-line) #" "))]))
+    (while true
+      (let [; the game lasts 24 days: 0-23
+            day (Integer/parseInt (read-line))
+            ; the base score you gain from the next COMPLETE action
+            nutrients (Integer/parseInt (read-line))
+            ; sun: your sun points
+            ; score: your current score
+            [sun score] (map #(Integer/parseInt %) (str/split (read-line) #" "))
+            ; oppSun: opponent's sun points
+            ; oppScore: opponent's score
+            ; oppIsWaiting: whether your opponent is asleep until the next day
+            [oppSun oppScore oppIsWaiting] (str/split (read-line) #" ")
+            oppSun (Integer/parseInt oppSun)
+            oppScore (Integer/parseInt oppScore)
+            oppIsWaiting (not= "0" oppIsWaiting)
+            ; the current amount of trees
+            numberOfTrees (Integer/parseInt (read-line))]
+        (dotimes [i numberOfTrees]
+          (let [; cellIndex: location of this tree
+                ; size: size of this tree: 0-3
+                ; isMine: 1 if this is your tree
+                ; isDormant: 1 if this tree is dormant
+                [cellIndex size isMine isDormant] (str/split (read-line) #" ")
+                cellIndex (Integer/parseInt cellIndex)
+                size (Integer/parseInt size)
+                isMine (not= "0" isMine)
+                isDormant (not= "0" isDormant)]))
+        (let [; all legal actions
+              numberOfPossibleActions (Integer/parseInt (read-line))]
+          (dotimes [i numberOfPossibleActions]
+            (let [; try printing something from here to start with
+                  possibleAction (read-line)]))
+          
+          ; (debug "Debug messages...")
+          
+          ; GROW cellIdx | SEED sourceIdx targetIdx | COMPLETE cellIdx | WAIT <message>
+          (output "WAIT"))))))
